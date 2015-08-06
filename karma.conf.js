7f66a23c6f475f81ad36eec25c33340e9577ab15
ozkey <ozkey@ozkey.com>
2015-08-06 16:06:05 +1000

    learn t test Karma Jasmine coverage

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'jsGenTests/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress','coverage'],
 
 
     // web server port
@@ -63,7 +64,8 @@ module.exports = function(config) {
       plugins : [
           'karma-junit-reporter',
           'karma-chrome-launcher',
-          'karma-jasmine'
+          'karma-jasmine',
+          'karma-coverage'
       ],
 
     // Continuous Integration mode
